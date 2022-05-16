36c64b12e532b57b30db4c8296cecf1651b780e5
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-16 23:27:34 +0200

    scaffold made

--- lib/src/core/my_app.dart
+++ lib/src/core/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sepp_note/src/screens/screen%201/components/home_app_bar.dart';
 import 'package:sepp_note/src/screens/screen%201/home_page_provider.dart';
 
 class MyApp extends StatelessWidget {
@@ -19,6 +20,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Column(
+            children: [
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  height: 50,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      children: const [
+                        Icon(Icons.menu_outlined),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: const HomeBottomAppBar(),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.white,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+            onPressed: () {},
+            tooltip: 'Increment Counter',
+            child: const Icon(Icons.add, color: Colors.orange, size: 45),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked),
+    );
   }
 }
