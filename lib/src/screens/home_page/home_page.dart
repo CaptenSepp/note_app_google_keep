b5d6b27dc2616277046ddf4361f22d9fb7ccc081
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-17 13:02:14 +0200

    organized

--- lib/src/screens/home_page/home_page.dart
+++ lib/src/screens/home_page/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 import 'package:sepp_note/src/screens/home_page/components/home_page_bottom_app_bar.dart';
 import 'package:sepp_note/src/screens/home_page/components/home_page_search_bar.dart';
 import 'package:sepp_note/src/screens/home_page/home_page_provider.dart';
+
+import 'components/home_page_staggered_grid.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -27,37 +28,11 @@ class _HomePage extends StatelessWidget {
       home: Scaffold(
           drawer: const Drawer(),
           body: Column(
-            children: [
-              const SizedBox(height: 50),
-              const HomePageSearchBar(),
+            children: const [
+              SizedBox(height: 50),
+              HomePageSearchBar(),
               // SizedBox(height: MediaQuery.of(context),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: StaggeredGrid.count(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                    children: List.generate(10, (index) {
-                      return Expanded(
-                        child: Container(
-                          decoration: const BoxDecoration(
-                              color: Colors.black26,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              )),
-                          child: Center(
-                            child: Text(
-                              'Item $index',
-                              style: const TextStyle(fontSize: 50),
-                            ),
-                          ),
-                        ),
-                      );
-                    }),
-                  ),
-                ),
-              ),
+              HomePageStaggeredGrid(),
             ],
           ),
           bottomNavigationBar: const HomePageBottomAppBar(),
