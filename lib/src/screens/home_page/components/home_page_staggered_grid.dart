64e125d4183e9e46a92d0e88f888845ea37be46f
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-17 13:07:41 +0200

    HomePageStaggeredGridProvider added

--- lib/src/screens/home_page/components/home_page_staggered_grid.dart
+++ lib/src/screens/home_page/components/home_page_staggered_grid.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
 
 class HomePageStaggeredGrid extends StatelessWidget {
-  const HomePageStaggeredGrid({
+  const HomePageStaggeredGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => HomePageStaggeredGridProvider(),
+      child: const _HomePageStaggeredGrid(),
+    );
+  }
+}
+
+class HomePageStaggeredGridProvider extends ChangeNotifier {}
+
+class _HomePageStaggeredGrid extends StatelessWidget {
+  const _HomePageStaggeredGrid({
     Key? key,
   }) : super(key: key);
 
