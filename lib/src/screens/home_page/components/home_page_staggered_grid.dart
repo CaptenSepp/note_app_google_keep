e3b692869aacf3d1d4cae70fa8b25138951f0c6d
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-17 13:02:36 +0200

    organized

---
+++ lib/src/screens/home_page/components/home_page_staggered_grid.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class HomePageStaggeredGrid extends StatelessWidget {
+  const HomePageStaggeredGrid({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: StaggeredGrid.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          children: List.generate(10, (index) {
+            return Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: Colors.grey),
+                  // color: Colors.black26,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  )),
+              child: Center(
+                child: Text(
+                  'Item $index',
+                  style: const TextStyle(fontSize: 50),
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
