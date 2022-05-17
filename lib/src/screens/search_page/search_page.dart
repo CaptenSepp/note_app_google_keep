3b785c4bc7ea6e7dba7b054854d8c818f07ba7fa
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-17 19:46:02 +0200

    searchPage searchbar added

--- lib/src/screens/search_page/search_page.dart
+++ lib/src/screens/search_page/search_page.dart
@@ -18,6 +18,40 @@ class _SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 110,
+            decoration: const BoxDecoration(color: Colors.black12),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back, size: 30),
+                      onPressed: () {},
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.55,
+                      child: const Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: TextField(
+                            decoration:
+                                InputDecoration(border: InputBorder.none),
+                            cursorHeight: 30,
+                            cursorColor: Colors.black),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
