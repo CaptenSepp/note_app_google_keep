5f88d425713f8b2a7d9c95a00b7aa2426693686c
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-17 19:24:32 +0200

    bottomappbar icons added

--- lib/src/screens/home_page/components/home_page_bottom_app_bar.dart
+++ lib/src/screens/home_page/components/home_page_bottom_app_bar.dart
@@ -7,16 +7,26 @@ class HomePageBottomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BottomAppBar(
+    return BottomAppBar(
       notchMargin: 5,
       clipBehavior: Clip.hardEdge,
-      shape: AutomaticNotchedShape(
+      shape: const AutomaticNotchedShape(
         RoundedRectangleBorder(borderRadius: BorderRadius.vertical()),
         RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(15)),
         ),
       ),
-      child: SizedBox(width: double.infinity, height: 60),
+      child: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: Row(children: [
+          IconButton(
+              icon: const Icon(Icons.check_box_outlined), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.draw_outlined), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.mic_none), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.photo_outlined), onPressed: () {}),
+        ]),
+      ),
     );
   }
 }
