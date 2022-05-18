c2653d2d41cf717189a6cb68f41633e07888a92b
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-18 12:16:28 +0200

    notemodel and other inside models

--- lib/src/models/note_model.dart
+++ lib/src/models/note_model.dart
@@ -1,7 +1,46 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
 class NoteModel {
+  final List<dynamic> noteElementsList;
   final String noteModelId;
-//TODO the value has to be given as creating the object
+  final String title;
+  final DateTime createdDate;
+  final TextCheckboxNoteModel? textCheckboxNote;
+  final ImageNoteModel? imageNote;
+  final RecordNoteModel? recordNote;
+
   NoteModel({
-    this.noteModelId = '',
+    required this.noteElementsList,
+    required this.title,
+    required this.noteModelId,
+    required this.createdDate,
+    this.textCheckboxNote,
+    this.imageNote,
+    this.recordNote,
   });
+}
+
+class RecordNoteModel {}
+
+class ImageNoteModel {
+  final Image image;
+
+  ImageNoteModel(this.image);
+}
+
+class TextCheckboxNoteModel {
+  final String text;
+  final bool isTextOrCheckbox;
+
+  TextCheckboxNoteModel({
+    required this.text,
+    //TODO how can i write it better to understand
+    this.isTextOrCheckbox = true,
+  });
+  // bool isTextOrCheckbox() {
+  //   bool result = true;
+  //   return result;
+  // }
 }
