2106118ee09875c0a9af57afd1b43f70ad08c934
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-21 12:07:25 +0200

    note maker provider

---
+++ lib/src/modules/home/state_managment/note_maker_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class NoteMakerProvider extends ChangeNotifier {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController noteController = TextEditingController();
+}
