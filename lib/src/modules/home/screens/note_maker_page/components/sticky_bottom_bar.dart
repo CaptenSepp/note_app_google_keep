9062c1d67fd8544cb232ac6da281f5f7841f9fdf
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-23 00:00:24 +0200

    sliding up panel controller

--- lib/src/modules/home/screens/note_maker_page/components/sticky_bottom_bar.dart
+++ lib/src/modules/home/screens/note_maker_page/components/sticky_bottom_bar.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class StickyBottomAppBar extends StatelessWidget {
+  final PanelController pc;
+
   const StickyBottomAppBar({
     Key? key,
+    required this.pc,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // final PanelController _pc;
     return CustomStickyBottomAppBar(
-        child: BottomAppBar(
-      elevation: 0,
-      color: Colors.transparent,
-      child: Row(
-        children: [
-          IconButton(
-              icon: const Icon(Icons.add_box_outlined), onPressed: () {}),
-          IconButton(
-              icon: const Icon(Icons.palette_outlined), onPressed: () {}),
-          const SizedBox(
-            width: 50,
-          ),
-          IconButton(icon: const Icon(Icons.turn_left_sharp), onPressed: () {}),
-          IconButton(
-              icon: const Icon(Icons.u_turn_right_sharp), onPressed: () {}),
-          const SizedBox(
-            width: 95,
-          ),
-          IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
-        ],
+      child: BottomAppBar(
+        elevation: 0,
+        color: Colors.transparent,
+        child: Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.add_box_outlined),
+              onPressed: () {
+                pc.open();
+              },
+            ),
+            IconButton(
+                icon: const Icon(Icons.palette_outlined), onPressed: () {}),
+            const SizedBox(
+              width: 50,
+            ),
+            IconButton(
+                icon: const Icon(Icons.turn_left_sharp), onPressed: () {}),
+            IconButton(
+                icon: const Icon(Icons.u_turn_right_sharp), onPressed: () {}),
+            const SizedBox(
+              width: 95,
+            ),
+            IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
 
